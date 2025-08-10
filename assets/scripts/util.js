38e30be9d07c2fcc67736421a38e954062a4a5ac
Alexander Voglsperger <alex@wtf-my-code.works>
2025-08-10 19:07:21 +0200

    Add "load more" for work experience
    Only show three entries by default

--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -5,11 +5,24 @@
 
 // This file contains small utility functions that are used throughout the website
 
+/// Perform certain actions when the DOM is fully loaded.
+document.addEventListener("DOMContentLoaded", () => {
+	const ldWorkBtn = document.getElementById('loadMoreWork');
+	if (ldWorkBtn) {
+		// Loads more work experience items when the "Load More" button is clicked.
+		ldWorkBtn.addEventListener('click', () => {
+			const hiddenWorkDivs = document.querySelectorAll('.work-experience-item.hidden');
+			hiddenWorkDivs.forEach(div => div.classList.remove('hidden'));
+			ldWorkBtn.style.display = 'none';
+		});
+	}
+});
+
 /// Updates the copyright year by appending the current year onto the "copyright-year".
 function updateCopyrightYear() {
-    const copyrightYearElement = document.getElementById('copyright-year');
+	const copyrightYearElement = document.getElementById('copyright-year');
 	const currentYear = new Date().getFullYear();
-    if (copyrightYearElement != null && !copyrightYearElement.textContent.includes(`${currentYear}`)) {
-        copyrightYearElement.textContent += ` - ${currentYear}`;
-    }
+	if (copyrightYearElement != null && !copyrightYearElement.textContent.includes(`${currentYear}`)) {
+		copyrightYearElement.textContent += ` - ${currentYear}`;
+	}
 }
