677cf91e001f7700b642a1f857df9f0963482431
Alexander Voglsperger <alex@wtf-my-code.works>
2025-08-11 20:21:26 +0200

    Use onclick attributes

--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -5,25 +5,22 @@
 
 // This file contains small utility functions that are used throughout the website
 
-/// Perform certain actions when the DOM is fully loaded.
-document.addEventListener("DOMContentLoaded", () => {
+/// Toggle visibility of more work entries
+function showMoreWork() {
 	const ldWorkBtn = document.getElementById('loadMoreWork');
-	if (ldWorkBtn) {
-		// Loads more work experience items when the "Load More" button is clicked.
-		ldWorkBtn.addEventListener('click', () => {
-			const hiddenWorkDivs = document.querySelectorAll('.work-experience-list .item.hidden');
-			hiddenWorkDivs.forEach(div => div.classList.remove('hidden'));
-			ldWorkBtn.style.display = 'none';
-		});
+	const hiddenWorkDivs = document.querySelectorAll('.work-experience-list .item.hidden');
+	hiddenWorkDivs?.forEach(div => div.classList.remove('hidden'));
+	if (ldWorkBtn != null) {
+		ldWorkBtn.style.display = 'none';
 	}
-});
+}
 
 /// Updates the copyright year by appending the current year onto the "copyright-year".
 function updateCopyrightYear() {
-	const copyrightYearElement = document.getElementById('copyright-year');
-	const currentYear = new Date().getFullYear();
-	if (copyrightYearElement != null && !copyrightYearElement.textContent.includes(`${currentYear}`)) {
-		copyrightYearElement.textContent += ` - ${currentYear}`;
+	const cYearElem = document.getElementById('copyright-year');
+	const curYear = new Date().getFullYear();
+	if (cYearElem != null && !cYearElem.textContent.includes(`${curYear}`)) {
+		cYearElem.textContent += ` - ${curYear}`;
 	}
 }
 
